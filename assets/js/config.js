21850b2c322ef05c42991622cf2f801da7f32762
Eta <pleasefckoff00@gmail.com>
2025-02-04 20:13:51 +0000

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,27 @@
+const BADGES = [
+  {
+    name: "HypeSquad Bravery", // Message that will appear when hovering over the badge
+    image: "assets/badges/hypesquad_bravery.svg",
+  },
+];
+
+const USER_ID = "1053570332339998801"; 
+const BACKGROUND_IMAGE = "assets/images/star.gif";
+const STATUS = {
+  dnd: {
+    name: "Do not disturb",
+    color: "#ed4245",
+  },
+  idle: {
+    name: "Busy",
+    color: "#faa81a",
+  },
+  online: {
+    name: "Online",
+    color: "#3ba55d",
+  },
+  offline: {
+    name: "Offline",
+    color: "#6e7985",
+  },
+};
